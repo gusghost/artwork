48eeae228f92b86e95dd5513baa2340c29a85724
gusghost <121400.prc55@gmail.com>
2017-10-30 20:56:42 +0900

    kakinaositeru

--- mkfile.py
+++ mkfile.py
@@ -6,17 +6,18 @@ from keras.optimizers import Adam
 import numpy as np
 from PIL import Image
 import os
+import random
 
 # 学習用のデータを作る.
 image_list = []
 label_list = []
-
+epoch_num = 200
 # ./data/train 以下のorange,appleディレクトリ以下の画像を読み込む。
-for dir in os.listdir("./train"):
+for dir in os.listdir("X:/library/document/卒業制作/first_prototype01/test"):
     if dir == ".DS_Store":
         continue
 
-    dir1 = "./train/" + dir
+    dir1 = "../../first_prototype01/test/" + dir
     label = 0
 
     if dir == "maru_b":  # appleはラベル0
@@ -32,6 +33,7 @@ for dir in os.listdir("./train"):
         if file != ".DS_Store":
             # 配列label_listに正解ラベルを追加(りんご:0 オレンジ:1)
             label_list.append(label)
+
             filepath = dir1 + "/" + file
 
             # [R,G,B]はそれぞれが0-255の配列。
@@ -42,7 +44,10 @@ for dir in os.listdir("./train"):
 
             # これ白黒画像だからか１ピクセルにつき一つの数値ですね
             # image = image.reshape(1, image.shape[0] * image.shape[1] * image.shape[2]).astype("float32")[0]
+            # tate to yoko tekina
+            random.shuffle(image)
             image = np.reshape(image, (1, 784))
+            # [[number1,n2, n3, ..., nn]]
             # print(image)
             # 出来上がった配列をimage_listに追加。
             image_list.append(image / 255.)
@@ -51,6 +56,8 @@ for dir in os.listdir("./train"):
 
 # kerasに渡すためにnumpy配列に変換。
 image_list = np.array(image_list)
+image_list = np.reshape(image_list, (16650, 784))
+# , 16650
 # print(image_list)
 # ラベルの配列を1と0からなるラベル配列に変更
 # 0 -> [1,0], 1 -> [0,1] という感じ。
@@ -58,13 +65,13 @@ Y = to_categorical(label_list)
 # モデルを生成してニューラルネットを構築
 model = Sequential()
 # input_dimは次元の数らしい。denseとinputの数は最初に設定した次元の数と一致している必要がある
-model.add(Dense(256, input_dim=156568))
+model.add(Dense(784, input_dim=784))
 model.add(Activation("relu"))
-model.add(Dropout(0.2))
+# model.add(Dropout(0.2))
 
 model.add(Dense(256))
 model.add(Activation("relu"))
-model.add(Dropout(0.2))
+# model.add(Dropout(0.2))
 
 model.add(Dense(4))
 model.add(Activation("softmax"))
@@ -73,17 +80,18 @@ model.add(Activation("softmax"))
 opt = Adam(lr=0.001)
 # モデルをコンパイル
 model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
-model.fit(image_list, Y, epochs=200, batch_size=100, validation_split=0.1)
+# validation_split ha
+model.fit(image_list, Y, epochs=epoch_num, batch_size=100, validation_split=0.1)
 
 # テスト用ディレクトリ(./data/train/)の画像でチェック。正解率を表示する。
 total = 0.
 ok_count = 0.
 
-for dir in os.listdir("./test"):
+for dir in os.listdir("X:/library/document/卒業制作/first_prototype01/test"):
     if dir == ".DS_Store":
         continue  # continue is nani
 
-    dir1 = "./test/" + dir
+    dir1 = "X:/library/document/卒業制作/first_prototype01/test/" + dir
     label = 0
 
     if dir == "maru_b":  # appleはラベル0
@@ -99,11 +107,17 @@ for dir in os.listdir("./test"):
         if file != ".DS_Store":
             label_list.append(label)  # 型宣言が無いとどうもやりづらいというか頭が混乱するなぁ
             filepath = dir1 + "/" + file
-            image = np.array(Image.open(filepath).resize((25, 25)))
+            # image = np.array(Image.open(filepath).resize((25, 25)))
             print(filepath)
             # image = image.transpose(2, 0, 1)
-            image = np.reshape(image, (1, 625))
-            result = model.predict_classes(np.array([image / 255.]))
+            image = np.reshape(image, (1, 784))
+            random.shuffle(image)
+            # print(image)
+            # なぜnp.arrayすると次元というかカッコが一つ増えるのか謎なんだが。が。
+            image = np.array(image / 255.).reshape(1, 784)
+            # print(image)
+            result = model.predict_classes(image)
+            # np.array([image / 255.]))
             print("label:", label, "result:", result[0])
 
             total += 1.
@@ -111,4 +125,5 @@ for dir in os.listdir("./test"):
             if label == result[0]:
                 ok_count += 1.
 
-print("seikai: ", ok_count / total * 100, "%")
+print("correct: ", ok_count / total * 100, "%")
+print("epochs: ", epoch_num)
